ef95b8d9281a870834b4418dbc8cce3a8cc454c0
sudo.park <sudo.park@kakaocorp.com>
2019-10-05 14:10:17 +0900

    Chapter2 CustomView 생성 및 결합, NSView View로 랩핑

---
+++ Chapter1/Chapter1/CircleImage.swift
@@ -0,0 +1,39 @@
+//
+//  CircleImage.swift
+//  Chapter1
+//
+//  Created by ParkHyunsoo on 2019/10/05.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    
+    let width: CGFloat
+    let height: CGFloat
+    
+    // view에 생성자 이용방식 -> 기존이랑 똑같음
+    
+    init(width: CGFloat = 200, height: CGFloat = 200) {
+        self.width = width
+        self.height = height
+    }
+    
+    var body: some View {
+        Image("sample")
+            .frame(width: width, height: height)
+            .scaledToFit()
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- Chapter1/Chapter1/ContentView.swift
+++ Chapter1/Chapter1/ContentView.swift
@@ -15,22 +15,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text("Turtle Rock")
-                // 여기서 아래 붙는놈들은 modifier
-                // Your code is always the source of truth for the view. -> inspector에서 변하건 modifier를 통해 변하건
-                .font(.largeTitle)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                // Spacer는 내용에의한 크기를 갖지 않고 상위뷰의 모든 공간을 채운다
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                // width가 정의되있지 않다면 contentSize로 자동 맞춤 -> 이경우 mapView가 가능한 모든 스페이스 차지
+                .frame(height: 300)
+            
+            CircleImage(width: 180, height: 180)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Turtle Rock")
+                    // 여기서 아래 붙는놈들은 modifier
+                    // Your code is always the source of truth for the view. -> inspector에서 변하건 modifier를 통해 변하건
+                    .font(.largeTitle)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    // Spacer는 내용에의한 크기를 갖지 않고 상위뷰의 모든 공간을 채운다
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Spacer()
+                .frame(height: 100)
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
