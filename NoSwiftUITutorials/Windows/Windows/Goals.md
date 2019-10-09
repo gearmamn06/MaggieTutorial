dadc8bed6000a532b84e09c4daf6bf91b42b64ed
sudo.park <sudo.park@kakaocorp.com>
2019-10-10 00:18:27 +0900

    Chapter4 multi windows
    - Project file added

---
+++ NoSwiftUITutorials/Windows/Windows/Goals.md
@@ -0,0 +1,11 @@
+#  Goals
+
+
+## You'll create multi-window document-based app inspired by TextEdit. In the process, you'll learn about:
+
+### 1. Windows and window controllers
+### 2. The Document architectures
+### 3. NSTextView
+### 4. Modal Windows
+### 5. The menu bar and menu items
+
---
+++ NoSwiftUITutorials/Windows/Windows/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WIndows
+//
+//  Created by ParkHyunsoo on 2019/10/10.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
