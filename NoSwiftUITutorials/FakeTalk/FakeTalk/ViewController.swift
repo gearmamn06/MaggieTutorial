8ae94909057c7ad4b7c9db60df89af14af72d7db
sudo.park <sudo.park@kakaocorp.com>
2019-10-11 00:40:54 +0900

    practice 전체 구조 및 goal 작성

---
+++ NoSwiftUITutorials/FakeTalk/FakeTalk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FakeTalk
+//
+//  Created by ParkHyunsoo on 2019/10/11.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
