1450a2ba43f83dcd1ba8266e0128612a34ef99c4
sudo.park <sudo.park@kakaocorp.com>
2019-10-24 22:12:05 +0900

    Chater2

---
+++ Tutorial2/P2_CowsAndBulls/P2_CowsAndBulls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  P2_CowsAndBulls
+//
+//  Created by ParkHyunsoo on 2019/10/24.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
