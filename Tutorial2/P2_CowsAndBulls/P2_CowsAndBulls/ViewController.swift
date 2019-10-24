fc4544fab6705a3854ab989f7a13b7b38df1a9f1
sudo.park <sudo.park@kakaocorp.com>
2019-10-24 23:07:14 +0900

    Chapter2 end

--- Tutorial2/P2_CowsAndBulls/P2_CowsAndBulls/ViewController.swift
+++ Tutorial2/P2_CowsAndBulls/P2_CowsAndBulls/ViewController.swift
@@ -9,19 +9,116 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var guess: NSTextField!
+    @IBOutlet weak var tableView: NSTableView!
+    
+    private var answer = ""
+    private var guesses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        startANewGame()
+        
+        tableView.tableColumns.first?.title = "Guess"
+        tableView.tableColumns.last?.title = "Result"
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    @IBAction func submitGuess(_ sender: Any) {
+        let input = self.guess.stringValue
+        if Set(input).count == 4 {
+            self.guesses.insert(input, at: 0)
+            self.guess.stringValue = ""
+            
+            self.tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
         }
     }
-
-
 }
 
+
+// MARK: Game logic
+
+extension ViewController {
+    
+    private func startANewGame() {
+        self.guess.stringValue = ""
+        self.guesses.removeAll()
+        self.answer = generateRandomAnswer()
+        
+        self.tableView.reloadData()
+    }
+    
+    private func generateRandomAnswer() -> String {
+        return (0..<4)
+            .map{ _ in Int.random(in: 0..<9) }
+            .reduce("", { str, v in
+                return "\(str)\(v)"
+            })
+        
+    }
+    
+    private func result(for input: String) -> String {
+        let ans = answer.map{ $0 }
+        let matches: (Int, Int) = input
+            .map{ $0 }
+            .reduce((0, 0), { tupple, c in
+                if ans.contains(c) {
+                    return (tupple.0 + 1, tupple.1)
+                }else{
+                    return (tupple.0, tupple.1 + 1)
+                }
+            })
+        if matches.0 == 4 {
+            showEndGame()
+            return "4b"
+        }
+        
+        return "\(matches.0)b \(matches.1)c"
+    }
+    
+    private func showEndGame() {
+        
+        DispatchQueue.main.async {
+            let alert = NSAlert()
+            alert.messageText = "You win"
+            alert.informativeText = "Click to Ok to play again"
+            
+            alert.runModal()
+            
+            self.startANewGame()
+        }
+    }
+}
+
+
+// MARK: NSTableView Datasource & Delegate
+
+extension ViewController: NSTableViewDataSource, NSTableViewDelegate {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.guesses.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let cell = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        switch tableColumn?.title {
+        case "Guess":
+            cell.textField?.stringValue = guesses[row]
+            
+        case "Result":
+            cell.textField?.stringValue = result(for: guesses[row])
+            
+        default: break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
+    }
+}
